345694a96287fc5e9f8fca6c972f92c6a1168093
Nick Borromeo <nickborromeo@Nicks-MBP.attlocal.net>
2021-06-18 00:45:56 -0700

    install all the things

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 DIR="$(pwd -P $0)"
 OS="$(uname)"
 
-if [ "$OS" = "Darwin" ]; then
-  if ! command -v brew &> /dev/null; then
-    echo 'Installing Homebrew...'
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  fi
-
-  echo 'Any packages today...?'
-  if ! brew bundle check; then
-    echo 'Bundling them up for you now...'
-    brew bundle install
-  fi
-fi
+#if [ "$OS" = "Darwin" ]; then
+#  if ! command -v brew &> /dev/null; then
+#    echo 'Installing Homebrew...'
+#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#  fi
+#
+#  echo 'Any packages today...?'
+#  if ! brew bundle check; then
+#    echo 'Bundling them up for you now...'
+#    brew bundle install
+#  fi
+#fi
 
 # Link all linkable files
 for linkable in $DIR/**/*.symlink; do
