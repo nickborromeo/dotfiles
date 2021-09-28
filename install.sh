aa44bbbee577138285b7d00a0addae5c8794ae96
Nick Borromeo <ngborromeo@gmail.com>
2021-09-28 12:14:42 -0700

    Add apt packages

--- install.sh
+++ install.sh
@@ -16,14 +16,10 @@ if [ "$OS" = "Darwin" ]; then
   fi
 fi
 
-if [[ ! -z "$CODESPACES" ]]; then
+if [ -n "$CODESPACES" ]; then
   # Install Packages
-  apt-get install -y fzf libfuse2 ruby-dev tmux ripgrep
-  mkdir -p $HOME/bin
-
-  curl -L https://github.com/sharkdp/bat/releases/download/v0.18.2/bat-v0.18.2-x86_64-unknown-linux-gnu.tar.gz | tar -xz -C $HOME
-  cp $HOME/bat-v0.18.2-x86_64-unknown-linux-gnu/bat $HOME/bin/.
-  rm -rf $HOME/bat-v0.18.2-x86_64-unknown-linux-gnu
+  echo 'Installing some packages into the Codespace...'
+  apt-get install -y fzf libfuse2 ruby-dev tmux ripgrep zsh bat
 fi
 
 # Link all linkable files
