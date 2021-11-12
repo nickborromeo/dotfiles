61d150081b52debfb9e93993dbe6b6f081b01991
Nick Borromeo <ngborromeo@gmail.com>
2021-11-11 17:48:22 -0800

    files copy

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,10 +3,11 @@
 DIR="$(pwd -P $0)"
 MUX_DIR="$HOME/.config/tmuxinator"
 
+echo "📋️ Copying tmuxinator templates..."
+mkdir -p $MUX_DIR
+cp $DIR/config/tmuxinator/*.yml $MUX_DIR
+
 # Install tmuxinator
 echo "Install tmuxinator..."
 gem install tmuxinator
 
-echo "📋️ Copying tmuxinator templates..."
-mkdir $MUX_DIR
-cp $DIR/**/*.yml $MUX_DIR
