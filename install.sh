30054330d504551e108c1e4d0437bae0dde39314
Nick Borromeo <nickborromeo@github.com>
2022-01-17 12:43:37 -0800

    add vim to codespace

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 if [ -n "$CODESPACES" ]; then
   # Install Packages
   echo 'Installing some packages into the Codespace...'
-  apt-get install -y fzf libfuse2 rbenv ruby-dev tmux ripgrep zsh bat
+  apt-get install -y fzf libfuse2 rbenv ruby-dev tmux ripgrep zsh bat vim
 fi
 
 # Link all linkable files
