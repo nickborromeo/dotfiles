a9b6c40241b84943cd4fbb10a0df5f4a70b1c114
Nick Borromeo <nickborromeo@github.com>
2022-07-25 15:17:22 -0700

    Add universal-ctags

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 if [ -n "$CODESPACES" ]; then
   # Install Packages
   echo 'Installing some packages into the Codespace...'
-  apt-get install -y fzf libfuse2 rbenv ruby-dev tmux ripgrep zsh bat vim-gtk xsel
+  apt-get install -y fzf libfuse2 rbenv ruby-dev tmux ripgrep zsh bat vim-gtk xsel universal-ctags
 fi
 
 # Link all linkable files
