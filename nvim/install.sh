39fcb8b95050f740dd99a7f337aaecac57a0c1e3
Nick Borromeo <nickborromeo@github.com>
2022-12-28 22:30:00 -0800

    more tweaks

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,11 +5,11 @@ DIR=$(pwd -P "$0")/nvim
 echo "Setting up NeoVim."
 
 if [ ! -d $HOME/.config/nvim ]; then
-  ln -sd "$DIR" $HOME/.config/nvim
+  ln -s "$DIR" $HOME/.config/nvim
 fi
 
 if command -v nvim > /dev/null; then
-  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+  nvim -u ~/.config/nvim/install.vim
 else
   echo "Whooops! No Neovim!"
 fi
