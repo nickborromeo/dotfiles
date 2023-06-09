217c4907fd6a85a27918c87b8772f5c894a59b52
Nick Borromeo <nickborromeo@github.com>
2023-06-08 17:49:53 -0700

    testing install of neovim

--- install.sh
+++ install.sh
@@ -30,13 +30,17 @@ if [ -n "$CODESPACES" ]; then
     libfuse2
 
   # Install neovim
-  NVIM_VERSION=0.8.1
-  curl -L -o $HOME/bin/nvim --create-dirs https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
-  chmod a+x $HOME/bin/nvim
+  #NVIM_VERSION=0.8.1
+  #curl -L -o $HOME/bin/nvim --create-dirs https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
+  #chmod a+x $HOME/bin/nvim
+
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  ./nvim.appimage
 
   # Vim Plug
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
 # Link all linkable files
