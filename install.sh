00f15f4a6325e196168f5eaed8eda4bd4adce7a2
Nick Borromeo <nickborromeo@github.com>
2025-09-21 01:29:24 -0700

    Set TERM environment variable for terminal compatibility

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ if [ -n "$CODESPACES" ]; then
   sudo mv squashfs-root /
   sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
+  export TERM="xterm-256color"
   # Vim Plug
   # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
